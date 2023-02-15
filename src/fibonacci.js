cf3d274519914dd85a0c080898c2de2c9868d6a1
Samson Nwokike <nwokikesamson@gmail.com>
2023-02-15 06:13:11 +0100

    fibonacci sequence

---
+++ src/fibonacci.js
@@ -0,0 +1,18 @@
+// Problem: Given a number n, find the first n elements of the Fibonacci sequence
+// Example: f(3) => [0,1,1]
+//Hints:
+// The first two numbers are 0 and 1.
+// Each number is the sum of the previous two numbers
+// The total length of a fibonacci is n number passed ie: if f(n), n === f(n).length
+
+function f(n) {
+  const fib = [0, 1];
+  while (fib.length < n) {
+    const nextNum = fib[fib.length - 1] + fib[fib.length - 2];
+    fib.push(nextNum);
+  }
+
+  return fib;
+}
+
+console.log(f(5));
